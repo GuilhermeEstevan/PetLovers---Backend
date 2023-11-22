0f309a5b2633622484bddbc012cfd51caa6e9216
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-11-22 19:14:00 -0300

    update

--- src/services/Pet/editPet.ts
+++ src/services/Pet/editPet.ts
@@ -1,43 +1,12 @@
 import BadRequestError from "../../errors/badRequest";
 import { TEditPetRequest, TPet } from "../../interfaces/pets";
 import PetModel from "../../models/pets";
-import { v2 as cloudinary } from "cloudinary";
-import { promises as fs } from "fs";
 
 const editPetService = async (
   data: TEditPetRequest,
   petId: string,
-  userId: string,
-  imageFile?: Express.Multer.File
+  userId: string
 ): Promise<TPet | null> => {
-  // CASO OCORRA UPLOAD DE NOVA FOTO
-  let imageUrl;
-
-  if (imageFile) {
-    // Verificar condições da imagem
-    if (!imageFile.mimetype.startsWith("image")) {
-      throw new BadRequestError("Please Upload an Image");
-    }
-
-    const maxSize = 2 * 1024 * 1024;
-
-    if (imageFile.size > maxSize) {
-      throw new BadRequestError(
-        `Please upload an image smaller than ${maxSize}KB`
-      );
-    }
-
-    const cloudinaryResponse = await cloudinary.uploader.upload(
-      imageFile.path,
-      {
-        use_filename: true,
-        folder: "PetLovers",
-      }
-    );
-    imageUrl = cloudinaryResponse.secure_url;
-    await fs.unlink(imageFile.path);
-  }
-
   const updatedData: TEditPetRequest = {
     name: data.name,
     species: data.species,
@@ -45,9 +14,10 @@ const editPetService = async (
     gender: data.gender,
     color: data.color,
     birthday: data.birthday,
-    photo: imageUrl || data.photo,
+    photo: data.photo,
   };
   console.log(updatedData);
+  console.log(petId);
 
   const UpdatedPet = await PetModel.findOneAndUpdate(
     {

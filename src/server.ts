b18aaeb19f77987e216d292057b283c5317a1960
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-12-01 19:43:08 -0300

    feat: https config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,18 @@ import app from "./app";
 import connectDB from "./DB/connect";
 import scheduleBirthdayReminder from "./Utils/Reminders/petBirthdayReminder";
 import scheduleVaccineReminder from "./Utils/Reminders/VaccineDueDateReminder";
+import https from "https";
+import fs from "fs";
+import path from "path";
+
+const privateKeyPath = "/etc/letsencrypt/live/www.petlovers.app.br/privkey.pem";
+const certificatePath =
+  "/etc/letsencrypt/live/www.petlovers.app.br/fullchain.pem";
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, privateKeyPath)),
+  cert: fs.readFileSync(path.resolve(__dirname, certificatePath)),
+};
 
 const port: number = Number(process.env.PORT) || 3000;
 
@@ -12,11 +24,16 @@ const start = async () => {
   }
   try {
     await connectDB(URI);
-    scheduleBirthdayReminder()
-    scheduleVaccineReminder()
-    app.listen(port, () => {
+    scheduleBirthdayReminder();
+    scheduleVaccineReminder();
+
+    https.createServer(options, app).listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
+
+    // app.listen(port, () => {
+    //   console.log(`Server is running on port ${port}`);
+    // });
   } catch (error) {
     console.log(error);
   }
